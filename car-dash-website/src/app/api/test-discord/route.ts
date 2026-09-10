c8e27ee738685ae97b195a953289075ce0809bb6
The Cute Axolotl <thecuteaxolotl@yahoo.com>
2026-09-10 11:58:29 -0500

    Update route.ts

--- car-dash-website/src/app/api/test-discord/route.ts
+++ car-dash-website/src/app/api/test-discord/route.ts
@@ -17,7 +17,7 @@ export async function GET() {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
-        content: "@everyone New detailing request received!",
+        content: "New detailing request received!",
       }),
     });
 
